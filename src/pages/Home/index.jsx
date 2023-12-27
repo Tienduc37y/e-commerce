e3ee4f0ee37d2585af1418a96ca89d4573ef4ebf
Tienduc37y <laitienduc37@gmail.com>
2023-12-27 21:38:59 +0700

    first commit

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,10 @@
+import './Home.css'
+import ProductList from '@/components/Product/ProductList'
+export default function Home(){
+    return (
+        <>
+            <ProductList />
+            <ProductList query="filters[idCategories][slug]=laptop-gaming"/>
+        </>
+    )
+}
